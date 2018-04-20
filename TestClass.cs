8d7c56ac1467f8404bd6d1b052f1164ab2935bec
seanfifebw <35851438+seanfifebw@users.noreply.github.com>
2018-04-20 08:02:20 -0700

    Test

--- TestClass.cs
+++ TestClass.cs
@@ -22,18 +22,5 @@ namespace CheckFieldPopulation
                 x = 2;
             }
         }
-
-        public void addThis()
-        {
-            int z = 3;
-            for (int i = 0; i < z; i++)
-            {
-            }
-
-            if (q > 3)
-            {
-                q = 2;
-            }
-        }
     }
 }
