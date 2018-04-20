33d399aa385275e1b6585d87713cce8e177e6e67
seanfifebw <35851438+seanfifebw@users.noreply.github.com>
2018-04-20 07:58:00 -0700

    Test

--- TestClass.cs
+++ TestClass.cs
@@ -9,13 +9,18 @@ namespace CheckFieldPopulation
     class TestClass
     {
         public void existingFunctionToDelete()
+        public void existingFunction()
         {
             int y = 2;
+            int x = 1;
         }
 
         public void existingFunction()
+        public void addThis()
         {
             int x = 1;
+            int z = 3;
+        }
         }
     }
 }
